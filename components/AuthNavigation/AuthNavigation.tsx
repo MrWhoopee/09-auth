3e01f23cfb43328bf4d3d963dea7b6095aa1eef8
Mr_Whoopee <artem.holovko.97@gmail.com>
2026-01-18 18:51:18 +0200

    Added user name to navigation

--- components/AuthNavigation/AuthNavigation.tsx
+++ components/AuthNavigation/AuthNavigation.tsx
@@ -7,7 +7,7 @@ import { logout } from "@/lib/api/clientApi";
 import { useRouter } from "next/navigation";
 
 export default function AuthNavigation() {
-  const { isAuthenticated, clearIsAuthenticated } = useAuthStore();
+  const { user, isAuthenticated, clearIsAuthenticated } = useAuthStore();
   const router = useRouter();
 
   const handleLogout = async () => {
@@ -29,7 +29,7 @@ export default function AuthNavigation() {
             </Link>
           </li>
           <li className={css.navigationItem}>
-            <p className={css.userEmail}>User email</p>
+            <p className={css.userEmail}>{user?.username ?? "User"}</p>
             <button className={css.logoutButton} onClick={handleLogout}>
               Logout
             </button>
